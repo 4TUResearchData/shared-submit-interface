20dcee5d39cb115caa59cd3123d9ae4c607aebc4
Roel Janssen <r.r.e.janssen@tudelft.nl>
2024-01-25 16:30:31 +0100

    database: Improve error handling of loading BerkeleyDB stores.
    * src/shared_submit_interface/database.py: Differentiate between
      corrupted store, no store, and a normally loaded store.

--- src/shared_submit_interface/database.py
+++ src/shared_submit_interface/database.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from urllib.error import URLError, HTTPError
 from rdflib import Dataset, Graph, Literal, RDF, XSD, URIRef
 from rdflib.plugins.stores import sparqlstore
-from rdflib.store import VALID_STORE
+from rdflib.store import VALID_STORE, CORRUPTED_STORE, NO_STORE
 from jinja2 import Environment, FileSystemLoader
 from shared_submit_interface import cache, rdf
 
@@ -37,8 +37,13 @@ class SparqlInterface:
             directory = self.endpoint[6:]
             self.sparql = Dataset("BerkeleyDB")
             self.sparql.open (directory, create=True)
-            if self.sparql != VALID_STORE:
-                self.log.error ("'%s' is not a valid BerkeleyDB database.", directory)
+            if not isinstance (self.sparql, Dataset):
+                if self.sparql == CORRUPTED_STORE:
+                    self.log.error ("'%s' is corrupted.", directory)
+                elif self.sparql == NO_STORE:
+                    self.log.error ("'%s' is not a BerkeleyDB store.", directory)
+                else:
+                    self.log.error ("Loading '%s' returned %s.", directory, self.sparql)
                 return None
             self.log.info ("Using BerkeleyDB RDF store.")
 
