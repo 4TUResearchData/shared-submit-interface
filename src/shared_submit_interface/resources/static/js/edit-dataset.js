749e10a1b4b9ba7f76e9d4230baf244fbfc5d5b3
Roel Janssen <r.r.e.janssen@tudelft.nl>
2024-01-31 16:25:31 +0100

    Implement autocompletion for the affiliation field.
    * src/shared_submit_interface/resources/html_templates/edit-dataset.html:
      Add a hidden input field to store the UUID of a completed affiliation.
    * src/shared_submit_interface/resources/static/js/edit-dataset.js:
      Implement autocompletion for the affiliation field.

--- src/shared_submit_interface/resources/static/js/edit-dataset.js
+++ src/shared_submit_interface/resources/static/js/edit-dataset.js
@@ -13,10 +13,9 @@ function show_message (type, message) {
 }
 
 function gather_form_data () {
-    console.log ("Gathering form data ..");
     let form_data = {
         "title":          or_null(jQuery("#title").val()),
-        "affiliation":    or_null(jQuery("#affiliation").val()),
+        "affiliation":    or_null(jQuery("#affiliation-uuid").val()),
         "email":          or_null(jQuery("#email").val()),
         //"description":    or_null(jQuery("#description .ql-editor").html()),
     };
@@ -45,12 +44,59 @@ function save_dataset (event, dataset_uuid, notify=true, on_success=jQuery.noop)
             show_message ("failure", "<p>Failed to save the draft. Please try again at a later time.</p>");
         }
     });
+}
 
-    console.log (`Gathered: ${JSON.stringify(form_data)}.`);
+function set_organization (event, dataset_uuid) {
+    let values = jQuery("input[name='organization']:checked").val().split(":");
+    let uuid = values[0];
+    let name = values[1];
+    jQuery("#affiliation-uuid").val(uuid);
+    jQuery("#affiliation").val(name);
+    jQuery("#affiliation-ac").remove();
+    jQuery("#affiliation").removeClass("input-for-ac");
+    jQuery("#email").focus();
+}
+function autocomplete_organization (event, dataset_uuid) {
+    if (event !== null) {
+        event.preventDefault();
+        event.stopPropagation();
+    }
+
+    let current_text = jQuery.trim(jQuery("#affiliation").val());
+    if (current_text == "") {
+        jQuery("#affiliation-ac").remove();
+        jQuery("#affiliation").removeClass("input-for-ac");
+        jQuery("#affiliation-uuid").val("");
+    } else if (current_text.length > 2) {
+        jQuery.ajax({
+            url:         `/organizations`,
+            type:        "POST",
+            contentType: "application/json",
+            accept:      "application/json",
+            data:        JSON.stringify({ "search_for": current_text }),
+            dataType:    "json"
+        }).done(function (data) {
+            jQuery("#affiliation-ac").remove();
+            let html = '<ul id="affiliation-list">';
+            if (data.length == 0) {
+                html += "<li>No affiliated organization found.";
+            }
+            for (let item of data) {
+                html += `<li><input type="radio" id="organization-${item['uuid']}" `;
+                html += `name="organization" value="${item['uuid']}:${item['name']}">`;
+                html += `<label class="no-head" for="organization-${item['uuid']}">${item['name']}</label>`;
+            }
+            html += "</ul>";
+            jQuery("#affiliation")
+                .addClass("input-for-ac")
+                .after(`<div id="affiliation-ac" class="autocomplete">${html}</div>`);
+            jQuery("#affiliation-list").on("change", function (event) { return set_organization (event, dataset_uuid); });
+        });
+    }
 }
 
 function activate (dataset_uuid) {
     jQuery("#save").on("click", function (event) { save_dataset (event, dataset_uuid); });
     jQuery("#transfer").on("click", function (event) { transfer_dataset (event, dataset_uuid); });
-    console.log("Activated event handlers.");
+    jQuery("#affiliation").on("input", function (event) { autocomplete_organization (event, dataset_uuid); });
 }
